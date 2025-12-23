26aac4eae8d7947883943e0db1d656f050975415
dip1905 <dipesh.patel9977@gmail.com>
2025-12-23 13:37:00 +0530

    Update JobForm.jsx

--- JobTracker/src/components/JobForm.jsx
+++ JobTracker/src/components/JobForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./JobForm.css";
+import "./Jobform.css";
 import { addJob } from "../services/api";
 
 const JobForm = () => {
